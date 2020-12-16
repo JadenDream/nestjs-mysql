0d44d519734efe753d22156c588c79813a57bdec
JadneDream <brady0416@gmail.com>
2020-12-16 17:52:49 +0800

    Update cats.controller.ts
    add validate pipe

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UsePipes } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './entities/cat.entity';
 import { CatsService } from './cats.service';
+import { ValidatePipe } from '../validate.pipe';
 
 @Controller('cats')
 export class CatsController {
@@ -13,8 +14,9 @@ export class CatsController {
 	findAll(): Promise<Cat[]> {
 		return this.catsService.findAll();
 	} 
-		
+		 
 	@Post()
+	@UsePipes(new ValidatePipe())
 	create(@Body() createCatDto: CreateCatDto): Promise<Cat> {
 		const tCat = {
 			id : null,
