5490fbaa4e77606f03ee633e2988f5e761b912cd
JadneDream <brady0416@gmail.com>
2020-12-16 17:52:52 +0800

    Update create-cat.dto.ts
    add validate pipe

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,6 +1,15 @@
+import { IsString, IsInt, IsBoolean } from 'class-validator';
+
 export class CreateCatDto {
+	@IsString()
     name: string;
+	
+	@IsInt()
     age: number;
+	
+	@IsString()
     color: string;
+	
+	@IsBoolean()
 	isActive: boolean
 }
